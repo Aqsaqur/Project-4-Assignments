a28be937e62916f0675cd3d9a61cfbad23a47439
Aqsa Qur <aqsaqurayshi@gmail.com>
2025-03-21 20:12:52 +0500

    assignments added

---
+++ Assignments 00 to 05/02_lists/06_get_last_element.py
@@ -0,0 +1,12 @@
+def get_last_element(lst):
+    """Prints the last element of the given list."""
+    print(lst[-1])
+
+def output():
+    # Example usage
+    sample_list = [1, 2, 3, 4, 5]  # You can change this to test different lists
+    get_last_element(sample_list)
+
+
+if __name__ == '__main__':
+    output()
